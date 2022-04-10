0067b144bf16e61ddf3f53d2d2f8e21729e99d77
Purevdorj Enkhjargal <epuujee@gmail.com>
2022-04-10 06:10:42 -0400

    nltk.download punk

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+nltk.download('punkt')
 
 st.title("Spam Filter - Course Project")
 st.header("GCIS 523 - Statistical Computing")
